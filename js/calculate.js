b362813909daf222ac1ade47a3ef96b5ee786e8f
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-02-18 12:27:51 +0600

    commiting for rectangle card

--- js/calculate.js
+++ js/calculate.js
@@ -20,6 +20,28 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     
 })
 
+//----------------------------------------------adding event handler for rectangle card----------------------------------------------
+document.getElementById('rectangle-button').addEventListener('click',function(){
+    //calling functions for getting converted values from string to number .
+    const convertedInputFirst = convertValueFromString('value-l');
+    const convertedInputSecond = convertValueFromString('value-w');
+
+
+    //appling formula of triangle
+    const rectangleArea = convertedInputFirst*convertedInputSecond;
+    console.log(rectangleArea);
+
+    //getting previous texts inside area of calculation
+    const areaForPrevResults = document.getElementById('area-for-results');
+    const areaForResults = areaForPrevResults.innerText;
+    
+    //calling functions for getting serial number inside area of calculation
+    var seriall = serial();
+    //adding new text with previous texts inside area of calculation
+    areaForPrevResults.innerText =  areaForResults + "\n"  + seriall + "." + "Rectangle = " + rectangleArea + "cm2";
+    
+})
+
 
 //---------------------------------------------------calling functions--------------------------------------------
 //function for serial number
