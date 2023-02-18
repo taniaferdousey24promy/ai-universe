1348e44b28322e4c735a6c61404c9f57622e0d79
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-02-18 12:33:46 +0600

    commiting for parallelogram card

--- js/calculate.js
+++ js/calculate.js
@@ -41,6 +41,27 @@ document.getElementById('rectangle-button').addEventListener('click',function(){
     areaForPrevResults.innerText =  areaForResults + "\n"  + seriall + "." + "Rectangle = " + rectangleArea + "cm2";
     
 })
+//----------------------------------------------adding event handler for parallelogram card----------------------------------------------
+document.getElementById('parallelogram-button').addEventListener('click',function(){
+    //calling functions for getting converted values from string to number .
+    const convertedInputFirst = convertValueFromString('value-base');
+    const convertedInputSecond = convertValueFromString('value-height');
+
+
+    //appling formula of triangle
+    const parallelogramArea = convertedInputFirst*convertedInputSecond;
+    console.log(parallelogramArea);
+
+    //getting previous texts inside area of calculation
+    const areaForPrevResults = document.getElementById('area-for-results');
+    const areaForResults = areaForPrevResults.innerText;
+    
+    //calling functions for getting serial number inside area of calculation
+    var seriall = serial();
+    //adding new text with previous texts inside area of calculation
+    areaForPrevResults.innerText =  areaForResults + "\n"  + seriall + "." + "parallelogram = " + parallelogramArea + "cm2";
+    
+})
 
 
 //---------------------------------------------------calling functions--------------------------------------------
