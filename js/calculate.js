218cd7679a2f46dc3d2c73ff27c791e3aac2c6fe
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-02-18 13:17:55 +0600

    commiting for pentagon card

--- js/calculate.js
+++ js/calculate.js
@@ -85,6 +85,28 @@ document.getElementById('rhombus-button').addEventListener('click',function(){
     
 })
 
+//----------------------------------------------adding event handler for pantagon card----------------------------------------------
+document.getElementById('pentagon-button').addEventListener('click',function(){
+    //calling functions for getting converted values from string to number .
+    const convertedInputFirst = convertValueFromString('value-perimeter');
+    const convertedInputSecond = convertValueFromString('value-apothem');
+
+
+    //appling formula of triangle
+    const rhombusArea = 0.5*convertedInputFirst*convertedInputSecond;
+    console.log(rhombusArea);
+
+    //getting previous texts inside area of calculation
+    const areaForPrevResults = document.getElementById('area-for-results');
+    const areaForResults = areaForPrevResults.innerText;
+    
+    //calling functions for getting serial number inside area of calculation
+    var seriall = serial();
+    //adding new text with previous texts inside area of calculation
+    areaForPrevResults.innerText =  areaForResults + "\n"  + seriall + "." + "Pantagon = " + rhombusArea + "cm2";
+    
+})
+
 
 //---------------------------------------------------calling functions--------------------------------------------
 //function for serial number
