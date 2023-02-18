f2f86fc6e2d98ca68e4ebb99885af9a4e448032b
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-02-18 12:40:57 +0600

    commiting for rhombus card

--- js/calculate.js
+++ js/calculate.js
@@ -63,6 +63,28 @@ document.getElementById('parallelogram-button').addEventListener('click',functio
     
 })
 
+//----------------------------------------------adding event handler for rhombus card----------------------------------------------
+document.getElementById('rhombus-button').addEventListener('click',function(){
+    //calling functions for getting converted values from string to number .
+    const convertedInputFirst = convertValueFromString('value-d1');
+    const convertedInputSecond = convertValueFromString('value-d2');
+
+
+    //appling formula of triangle
+    const rhombusArea = 0.5*convertedInputFirst*convertedInputSecond;
+    console.log(rhombusArea);
+
+    //getting previous texts inside area of calculation
+    const areaForPrevResults = document.getElementById('area-for-results');
+    const areaForResults = areaForPrevResults.innerText;
+    
+    //calling functions for getting serial number inside area of calculation
+    var seriall = serial();
+    //adding new text with previous texts inside area of calculation
+    areaForPrevResults.innerText =  areaForResults + "\n"  + seriall + "." + "Rhombus = " + rhombusArea + "cm2";
+    
+})
+
 
 //---------------------------------------------------calling functions--------------------------------------------
 //function for serial number
